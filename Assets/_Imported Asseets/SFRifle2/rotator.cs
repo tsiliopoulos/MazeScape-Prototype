781f453d57f4de1acffa9b653d78615154bbcd0f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-11-22 12:40:35 -0500

    updated project

---
+++ Assets/_Imported Asseets/SFRifle2/rotator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotator : MonoBehaviour {
+    public float xSpeed = 0.0f;
+    public float ySpeed = 1.0f;
+    public float zSpeed = 0.0f;
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void Update()
+    {
+        transform.Rotate(
+             xSpeed * Time.deltaTime,
+             ySpeed * Time.deltaTime,
+             zSpeed * Time.deltaTime
+        );
+    }
+}
